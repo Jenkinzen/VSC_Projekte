04444c36e48fcd366b2f89d2c744f070e2094cec
Gordon <ggordonhuesch94@gmail.com>
2026-02-28 23:25:50 +0100

    Suchfunktion für Rezeptnamen kommentiert

--- Java Projekte/Java Training/recipeprogram/src/main/service/Service.java
+++ Java Projekte/Java Training/recipeprogram/src/main/service/Service.java
@@ -7,17 +7,27 @@ import main.storage.Storage;
 
 public class Service 
 {
-    //wieso private storage?
-    private Storage storage = new Storage();
-
+    
+    final Storage storage = new Storage();
+    // Rückgabetyp der Funktion eine List(e) die Strings speichert
+    // (diese Liste existiert noch nicht, es wird nur gesagt das hier ein raus kommt)
+    // suchwort > parametervariable
     public List<String> filterNachRezeptName(String suchwort)
-    {
+    {   // hier wird die wahre Liste erstellt und kriegt "namen" als Variable
+        // ArrayList -> flexible Liste wo geadded und removed werden kann( und geändert ).
         List<String> namen = new ArrayList<>();
+        //Rezept = Element der Liste|r = variable
+        //storage.getRezepte() = Methode die alle Rezepte liefert
+        //für jedes Rezept r aus der Liste storage.getRezepte
         for (Rezept r : storage.getRezepte()) {
+        //gibste mir dat Rezept wo in dem Namen das steht was ich als 
+        //suchwort eingegeben hab
             if (r.getName().toLowerCase().contains(suchwort.toLowerCase())){
+                // und addest das in meine Liste "namen"
                 namen.add(r.getName());
             }
         }
+        // und dann gibste mir den Zettel mit der Liste
         return namen;
     }
 }
