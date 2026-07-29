3661fe0bed3f4c7bdff398b6edeb37c51213cae7
Gordon <ggordonhuesch94@gmail.com>
2026-07-30 00:09:27 +0200

    weitere sql repository tests integriert

--- Python Projekte/Rezeptprogramm/tests/test_sql_repository.py
+++ Python Projekte/Rezeptprogramm/tests/test_sql_repository.py
@@ -20,9 +20,41 @@ def sql_repo():
 
     connection.close()
 
-def test_add_and_all(sql_repo,test_rezept):
-    created_recipe = sql_repo.add(test_rezept)
 
-    sql_repo.all()
+def test_add_and_all(sql_repo,test_rezept):
+
+    testrezept = sql_repo.add(test_rezept)
 
     assert len(sql_repo.all()) == 1
+
+    assert test_rezept.name == "Test_Rezept 1"
+
+    assert len([r.name for r in test_rezept.zutaten]) == 2
+
+    assert testrezept.rezept_id == 1
+
+
+def test_remove(sql_repo,test_rezept):
+
+    sql_repo.add(test_rezept)
+
+    assert len(sql_repo.all()) == 1
+
+    sql_repo.remove(test_rezept)
+
+    assert len(sql_repo.all()) == 0
+
+def test_update(sql_repo,test_rezept):
+    testrezept = sql_repo.add(test_rezept)
+
+    testrezept.name = "Geändertes_Test_Rezept 1"
+    testrezept.gang = "Geänderter_Gang 1"
+    testrezept.rezept_id = 5
+
+    sql_repo.update(test_rezept)
+
+    assert testrezept.name == "Geändertes_Test_Rezept 1"
+    assert testrezept.gang == "Geänderter_Gang 1"
+    assert [rezept.rezept_id for rezept in sql_repo.all()] == [1]
+    # gleicher test aber einfacher, ohne lc über Index
+    assert sql_repo.all()[0].rezept_id == 1
